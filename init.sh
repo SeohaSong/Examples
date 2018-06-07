25d316c56925c26a6016c70acf415dff9eb04af8
seohasong <tisutoo@gmail.com>
2018-06-07 11:09:23 +0900

    update origin master

--- init.sh
+++ init.sh
@@ -1,9 +1,15 @@
 (
+    if [ "$home_path" = "" ]
+    then
+        echo "[$0] Need SEOHASONG OS."
+        exit
+    fi
+
     if [ "$1" != "" ]
     then
         touch __init__.py
         touch module.py
-        cp ~/SEOHASONG/.gitignore .
+        cp $home_path/SEOHASONG/.gitignore .
         echo "# $1" > README.md
         rm -rf init.sh .git
         mv ../template.module ../$1
