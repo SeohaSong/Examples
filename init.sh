119ef4c4164b089b22f53ce6187c214bbc926f16
seohasong <tisutoo@gmail.com>
2018-06-07 11:58:33 +0900

    update origin master

--- init.sh
+++ init.sh
@@ -8,15 +8,21 @@
     if [ "$1" = "" ]
     then
         echo "[$0] . init.sh <module_name>"
-        exit
-    else
-        touch __init__.py
-        touch module.py
-        cp $home_path/SEOHASONG/.gitignore .
-        echo "# $1" > README.md
-        rm -rf init.sh .git
-        mv ../template.module ../$1
     fi
+
+    mkdir module && touch module/init.py
+    echo \
+"import os
+os.system('git submodule init')
+os.system('git submodule update')"\
+    > module/init.py
+    echo "from .module import init" > __init__.py
+    touch module.py
+    cp $home_path/SEOHASONG/.gitignore .
+    echo "# $1" > README.md
+
+    rm -rf init.sh .git
+    mv ../template.module ../$1
 )
 
 if [ -f "__init__.py" ]
